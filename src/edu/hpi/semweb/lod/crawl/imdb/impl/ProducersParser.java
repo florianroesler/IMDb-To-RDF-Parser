8ce9c97dbeb6f534e975e7953cd0143d096c3529
LWW <ludwig.wall@student.hpi.uni-potsdam.de>
2014-05-20 10:34:14 +0200

    * finished producer parser

--- src/edu/hpi/semweb/lod/crawl/imdb/impl/ProducersParser.java
+++ src/edu/hpi/semweb/lod/crawl/imdb/impl/ProducersParser.java
@@ -1,5 +1,8 @@
 package edu.hpi.semweb.lod.crawl.imdb.impl;
 
+import java.util.List;
+
+import edu.hpi.semweb.lod.crawl.imdb.CleaningHelper;
 import edu.hpi.semweb.lod.crawl.imdb.IMDBGenericPersonParser;
 import edu.hpi.semweb.lod.crawl.imdb.RegexHelper;
 
@@ -8,30 +11,41 @@ public class ProducersParser extends IMDBGenericPersonParser{
 	private String localType;
 	private String currentFilm;
 	private String producer;
-	private boolean tripleComplete;
 	
 	@Override
 	protected void onNewLine(String line) {
-	
+		line.replaceAll("\\{\\(\\d+-\\d+-\\d+\\)\\}","");
 		if(line.contains("{")) return;
 		
-		if(line.startsWith("\t\t\t")){
+		if(line.startsWith("\t")){
 			currentFilm = line;
-			currentFilm = currentFilm.replace("{{SUSPENDED}}", "").replace("(TV)", "").replace("(V)", "").replaceAll("\\(\\d+\\)", "").replace("(VG)", "").replace("(????)", "");
-			if ((localType = RegexHelper.returnGroup(line, "\\((.*?producer.*?)\\)", 1)) != "") {
+			currentFilm = currentFilm.replace("{{SUSPENDED}}", "").replace("(TV)", "").replace("(V)", "").replaceAll("\\(\\d+\\)", "").replace("(VG)", "").replace("(????)", "").replaceAll("\t","").replace("\"","").trim();
+			if ((localType = RegexHelper.returnGroup(currentFilm, "\\(([^\\(]*producer[^\\)]*)\\)", 1)) != "") {
 				type = localType;
 			}
 			else {
 				type = "producer";
 			}
-			tripleComplete = true;
+			currentFilm = currentFilm.replaceAll("\\(.*?\\)","");
 		
 		}else{
-			producer = line.replace("{{SUSPENDED}}", "").replace("(TV)", "").replace("(V)", "").replaceAll("\\(\\d+\\)", "").replace("(VG)", "").replace("(????)", "").trim();
-			tripleComplete = false;
+			List<String> tiles = CleaningHelper.removeEmptyElements(line.split("\t"));
+			if(tiles.size() == 2){		
+				//thats a new producer and a trailing movie
+				producer = CleaningHelper.cleanActorName(tiles.get(0));
+				currentFilm = tiles.get(1);
+				producer = producer.replace("{{SUSPENDED}}", "").replace("(TV)", "").replace("(V)", "").replaceAll("\\(\\d+\\)", "").replace("(VG)", "").replace("(????)", "").replaceAll("\\(.*?\\)","").trim();
+			}
+			if ((localType = RegexHelper.returnGroup(currentFilm, "\\(([^\\(]*producer[^\\)]*)\\)", 1)) != "") {
+				type = localType;
+			}
+			else {
+				type = "producer";
+			}
+			currentFilm = currentFilm.replace("{{SUSPENDED}}", "").replace("(TV)", "").replace("(V)", "").replaceAll("\\(\\d+\\)", "").replace("(VG)", "").replace("(????)", "").replaceAll("\t","").replace("\"","").replaceAll("\\(.*?\\)","").trim();
 		}	
 	
-	if (tripleComplete) writeCSV(producer, type, currentFilm);
+	writeCSV(producer, type, currentFilm);
 		
 	}
 	
